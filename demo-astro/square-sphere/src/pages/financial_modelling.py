cd88971a38bfeb8c1b7b15bd3dcb718e936d2880
Hamza Safdar <hamzasafdar0023@gmail.com>
2023-01-26 13:13:59 +1100

    Update financial_modelling.py

--- demo-astro/square-sphere/src/pages/financial_modelling.py
+++ demo-astro/square-sphere/src/pages/financial_modelling.py
@@ -58,9 +58,7 @@ class MCSimulation:
         
         # Run the simulation of projecting stock prices 'nSim' number of times
         for n in range(self.nSim):
-        
-            if n % 10 == 0:
-                continue
+
             # Create a list of lists to contain the simulated values for each stock
             simvals = [[p] for p in last_prices]
     
