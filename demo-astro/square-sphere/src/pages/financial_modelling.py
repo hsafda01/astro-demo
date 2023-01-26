2cd3bb7ed314ba53b1b37c4cd8c5960950530d5a
Hassan Mehmood <95397062+mehassanhmood@users.noreply.github.com>
2023-01-25 23:21:34 -0500

    Update financial_modelling.py

--- demo-astro/square-sphere/src/pages/financial_modelling.py
+++ demo-astro/square-sphere/src/pages/financial_modelling.py
@@ -136,24 +136,4 @@ class MCSimulation:
         ci_series = self.confidence_interval
         ci_series.index = ["95% CI Lower","95% CI Upper"]
         return metrics.append(ci_series)
-spy_ticker = yf.Ticker('SPY')
-spy = spy_ticker.history(period = 'max')
 
-agg_ticker = yf.Ticker('AGG')
-agg = agg_ticker.history(period = 'max')
-df = pd.concat([spy,agg],axis = 'columns',keys =['spy','agg'],join = 'inner')
-df.rename(columns = {'Close':'close'},inplace = True)
-df.to_csv('stock_data_final.csv',index = False)
-data = pd.read_csv('stock_data_final.csv')
-weights = [.25,.25,.25,.25]
-simulation = MCSimulation(portfolio_data = df,  weights= weights , num_simulation = 500 ,num_trading_days = 252)
-simulation.calc_cumulative_return()
-simulation.plot_simulation()
-simulation.plot_distribution()
-stats = simulation.summarize_cumulative_return()
-initial_investment = 20000
-ci_upper = stats[9]*initial_investment
-ci_lower = stats[8]*initial_investment
-mean_return = round((((stats[1]*initial_investment)-initial_investment)/initial_investment)*100,2)
-print(f'Expected portfolio returns with 95% upper confidence interval : {round(ci_upper,2)}')
-print(f'Expected portfolio returns with 95% lower confidence interval : {round(ci_lower,2)}')
